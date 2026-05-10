a39beb590ac2594bbfd7b943cdbcdcfffbbaf629
arushi-codes <bhainsoraarushi@gmail.com>
2026-05-11 02:14:13 +0530

    Increase page load timeout for CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeFiles(on, config) {},
+    pageLoadTimeout: 120000,
   },
 });
